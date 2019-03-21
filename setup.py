2c1281ad6cd85a8c545583db62ae1e5bd0cf6998
SB <admin@openwifi.su>
2019-03-21 12:56:21 +0500

    some fix setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from distutils.core import setup
 setup(
     name='django_spell_checker',
     version='1',
-    packages=['json',],
     url='https://github.com/saintbyte/django-spell-checker',
     license='BSD',
     author='sb',
     author_email='admin@ural.im',
-    description=''
+    description='',
+    install_requires=[
+        'Django>=2.0',
+        'json',
+    ],
+    python_requires=">=3.5",
 )
