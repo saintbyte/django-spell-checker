726c7f62fa4f37c487390116de36668916c15d40
SB <admin@openwifi.su>
2019-03-21 16:06:53 +0500

    some fix setup.py and Manifest.in

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
+import setuptools
+import setuptools.command.test
 
-setup(
+setuptools.setup(
     name='django_spell_checker',
     version='1.0',
     url='https://github.com/saintbyte/django-spell-checker',
@@ -11,6 +13,7 @@ setup(
     install_requires=[
         'Django>=2.0',
     ],
+    packages=setuptools.find_packages(exclude=['ez_setup', 't', 't.*']),
     zip_safe=False,
     python_requires=">=3.5",
 )
